7c5a29b5998d4a583183a49d5868337d94597717
MikeWang <hi.from.mikewang@gmail.com>
2019-04-09 09:37:06 +1200

    Ad Activity Test

--- app/src/androidTest/java/com/mike/android/espresso/guide/MainActivityTest.kt
+++ app/src/androidTest/java/com/mike/android/espresso/guide/MainActivityTest.kt
@@ -1,5 +1,10 @@
 package com.mike.android.espresso.guide
 
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import org.junit.Rule
@@ -13,5 +18,13 @@ class MainActivityTest {
 
     @Test
     fun greet() {
+        // 1, TextView is empty
+        onView(withId(R.id.greeting)).check(matches(withText("")))
+
+        // 2, Click the button
+        onView(withId(R.id.greet_button)).perform(click())
+
+        // 3, TextView displays "Hello"
+        onView(withId(R.id.greeting)).check(matches(withText(R.string.hello)))
     }
 }
