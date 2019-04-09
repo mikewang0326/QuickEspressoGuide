184d86048bb7f5c0f083e72e3e4462b52b17e1dc
MikeWang <hi.from.mikewang@gmail.com>
2019-04-10 11:36:05 +1200

    Add RecyclerView related code

--- app/src/main/java/com/mike/android/espresso/guide/MainActivity.kt
+++ app/src/main/java/com/mike/android/espresso/guide/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.mike.android.espresso.guide
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.TextView
 
@@ -13,14 +16,38 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    setTitle(R.string.title)
+    initTitlebar()
+    initGreet()
+    initRecycleView()
+  }
 
+  private fun initGreet() {
     greetingView = findViewById(R.id.greeting)
     greetButton = findViewById(R.id.greet_button)
+  }
+
+  private fun initTitlebar() {
+    setTitle(R.string.title)
   }
 
   fun greet(v: View) {
     greetButton.isEnabled = false
     greetingView.setText(R.string.hello)
+  }
+
+  private fun initRecycleView() {
+    val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+    val footer = findViewById<TextView>(R.id.footer)
+    footer.setBackgroundColor(Color.LTGRAY)
+    footer.visibility = View.VISIBLE
+
+    recyclerView.setHasFixedSize(true)
+    recyclerView.layoutManager = LinearLayoutManager(this)
+    recyclerView.adapter = NumberAdapter(30, object : NumberAdapter.OnItemClickListener {
+      override fun onItemClick(position: Int) {
+        footer.text = position.toString()
+        footer.visibility = View.VISIBLE
+      }
+    })
   }
 }
